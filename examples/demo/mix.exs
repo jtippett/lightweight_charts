79591d9f2cdcb94bcc387e1185eee32817b657e9
James Tippett <james.tippett@gmail.com>
2026-03-25 19:40:53 +0700

    fix: switch demo app to Hex dep for lightweight_charts

--- examples/demo/mix.exs
+++ examples/demo/mix.exs
@@ -59,7 +59,7 @@ defmodule Demo.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.2.0"},
       {:bandit, "~> 1.5"},
-      {:lightweight_charts, path: "../.."}
+      {:lightweight_charts, "~> 0.1.0"}
     ]
   end
 
